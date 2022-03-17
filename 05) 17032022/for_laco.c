2e6477401ae5d1579478a39d772fbf078d2a9bf3
Orlando Alves <orlandoabneto@gmail.com>
2022-03-17 10:34:30 -0300

    Estrutura de Laco (For, While e DO WHILE)

---
+++ 05) 17032022/for_laco.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    system("clear");
+
+    for (i=1; i<=10; i++){
+
+        printf("%d\n", i);
+    }
+}
---
+++ 05) 17032022/while_laco.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int i=0;
+    system("clear");
+    
+    
+    while(i < 10){
+        
+        i = i+1;
+        printf("%d\n", i);
+    }
+    
+}
